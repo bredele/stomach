94917115be9d76086590fface241b1606efd1579
bredele <olivier.wietrich@gmail.com>
2014-11-09 11:33:39 -0700

    add test to clone node

--- test/stomach.js
+++ test/stomach.js
@@ -7,6 +7,7 @@ var assert = require('assert');
 var dom = require('stomach');
 
 describe("string", function() {
+
 	it("should render string template", function() {
 		var result = dom('<button>stomach</button>');
 		assert.equal(result.nodeName, 'BUTTON');
@@ -21,9 +22,18 @@ describe("string", function() {
 });
 
 describe("node", function() {
+
 	it("should return node", function() {
 		var btn = document.querySelector('.myButton');
 		var result = dom(btn);
 		assert.equal(btn, result);
+	});
+
+	it("should clone node", function() {
+		var btn = document.querySelector('.myButton');
+		var result = dom(btn, true);
+
+		btn.innerHTML = 'hello';
+		assert.equal(result.innerHTML, 'my button');
 	});
 });

5febda1b736746dfb11bc73db1d17ac91d5cc082
bredele <olivier.wietrich@gmail.com>
2014-11-09 11:43:28 -0700

    clone node option;

--- index.js
+++ index.js
@@ -2,28 +2,35 @@
 /**
  * Stomach constructor.
  *
- * Generate dom from string or html
- * node.
+ * Returns dom node from string
+ * or html element. It also can
+ * clone the returned node.
  *
  * Examples:
  *
+ *   // transform
  *   stomach('<button>hello</button>');
  *   stomach('#hello');
  *   stomach(node);
+ *
+ *   // clone
+ *   stomach('#hello', true);
  *   
  * @param {String | Element} tmpl
+ * @param {Boolean} bool (only when tmpl is Element)
  * @return {Element}
  * @api public
  */
 
-module.exports = function(tmpl) {
+module.exports = function(tmpl, bool) {
   if(typeof tmpl === 'string') {
-     if(tmpl[0] === '<') {
-       var div = document.createElement('div');
-       div.insertAdjacentHTML('beforeend', tmpl);
-       return div.firstChild;
-     } 
-     return document.querySelector(tmpl);
-   }
-   return tmpl;
+    if(tmpl[0] === '<') {
+      var div = document.createElement('div');
+      div.insertAdjacentHTML('beforeend', tmpl);
+      return div.firstChild;
+    } 
+    return document.querySelector(tmpl);
+  }
+  if(bool) tmpl = tmpl.cloneNode(true);
+  return tmpl;
 };
